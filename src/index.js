29550963c9bec2627d0e92fa53d2e88ebea84ccd
Ivan Munandar <3332190035@untirta.ac.id>
2024-06-30 22:31:42 +0700

    schema prisma v1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const session = require('express-session');
 require("dotenv").config();
+
+//auth routes
+const authRoutes = require('./route/authentication/userRoute');
 
 
 const PORT = process.env.PORT || 3001;
 const main = express();
+
+main.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: 'auto'
+    }
+}));
 
 main.use(cors());
 main.use(bodyParser.json());
 main.use(bodyParser.urlencoded({
     extended: false
 }));
+
+//use auth routes
+main.use('myg/auth', authRoutes);
 
 main.listen(PORT, () => {
     console.log('Server is running! port: ' + PORT);
