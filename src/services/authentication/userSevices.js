e80ec813968e9ba86d92a73ab2cfb4fc1df17d9d
Ivan Munandar <3332190035@untirta.ac.id>
2024-07-24 00:19:48 +0700

    Add API User

--- src/services/authentication/userSevices.js
+++ src/services/authentication/userSevices.js
@@ -2,26 +2,55 @@ const { database } = require('../../helpers/config/db');
 const argon2 = require('argon2');
 
 async function getAllUsers() {
-    return await database.user.findMany();
+    return await database.user.findMany({
+        include: {
+            userProfiles: true,
+        }
+    });
 }
 
 async function getUserById(userId) {
     return await database.user.findUnique({
+        where: { userId },
+        include: {
+            userProfiles: true,
+        }
+    });
+}
+
+async function updateUser(userId, userProfile) {
+    const { fullName, phoneNumber, birthdate, socialMedia, address, studioName, ktpPicture, studioLogo } = userProfile;
+    
+    // Cari user profile dengan userId
+    const userProfileData = await database.userProfile.findFirst({
         where: { userId }
     });
-}
 
-async function updateUser(userId, data) {
-    if (data.password) {
-        data.password = await argon2.hash(data.password);
+    if (!userProfileData) {
+        throw new Error('UserProfile not found');
     }
-    return await database.user.update({
-        where: { userId },
-        data
+
+    return await database.userProfile.update({
+        where: { profileId: userProfileData.profileId },
+        data: {
+            fullName,
+            phoneNumber,
+            birthdate: new Date(birthdate),
+            socialMedia,
+            address,
+            studioName: studioName || null,
+            ktpPicture: ktpPicture || null,
+            studioLogo: studioLogo || null,
+            updatedAt: new Date(),
+        }
     });
 }
 
+
 async function deleteUser(userId) {
+    await database.userProfile.deleteMany({
+        where: { userId }
+    });
     return await database.user.delete({
         where: { userId }
     });
