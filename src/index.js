e3294ca840596986bbd604de4a25061c73462dad
Ivan Munandar <3332190035@untirta.ac.id>
2024-07-23 13:33:19 +0700

    setting api upload file

--- src/index.js
+++ src/index.js
@@ -4,15 +4,25 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 require("dotenv").config();
 const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+const main = express();
+
+// Middleware untuk menangani unggahan file
+main.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+
+// Middleware logging untuk permintaan ke rute statis (opsional, untuk debugging)
+main.use('/uploads', (req, res, next) => {
+    console.log(`Request to static file: ${req.path}`);
+    next();
+});
 
 //auth routes
 const userRoutes = require('./route/authentication/userRoute');
 const layananRoutes = require('./route/myBeauticaRoute/layananRoutes');
 const testimoniRoutes = require('./route/myBeauticaRoute/testimoniRoute');
 
-
-const PORT = process.env.PORT || 3001;
-const main = express();
 
 main.use(cors());
 main.use(bodyParser.json());
