e4aa3d15502939dd6ea899f0e21af48977d6440e
Ivan Munandar <3332190035@untirta.ac.id>
2024-07-31 08:51:22 +0700

    change localhost into server IP

--- src/route/myAcademyRoute/trainingRoutes.js
+++ src/route/myAcademyRoute/trainingRoutes.js
@@ -5,7 +5,7 @@ const trainingController = require('../../controllers/myAcademyController/traini
 
 // Middleware untuk menambahkan header CORS
 router.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "http://92.112.192.81:3000");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type");
     next();
